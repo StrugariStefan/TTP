dccb0672a2885367925cc00e3a0128655a4a6dd2
Strugari, Stefan <Stefan.Strugari@centric.eu>
2020-03-20 22:58:44 +0200

    Add ttp domain specific implementation with no constrains

--- reader.py
+++ reader.py
@@ -1,16 +1,23 @@
 import json
 
 class ProblemInstance :
-    def __init__(self, capacity: float, no_cities: int, distances: list, values: list):
+    def __init__(self, capacity: float, no_cities: int, no_items: int, distances: list, values: list, weights: list, v_min: float, v_max: float, item_city_assignment: list) :
         self.capacity = capacity
         self.no_cities = no_cities
         self.distances = distances
         self.values = values
+        self.weights = weights
+        self.v_min = v_min
+        self.v_max = v_max
+        self.no_items = no_items
+        self.item_city_assignment = item_city_assignment
+    def __repr__(self) -> str:
+        return str(self.__dict__)
 
-def object_decoder(obj):
-    return ProblemInstance(obj['capacity'], obj['no_cities'], obj['distances'], obj['values'])
+def object_decoder(obj) -> ProblemInstance:
+    return ProblemInstance(obj['capacity'], obj['no_cities'], obj['no_items'], obj['distances'], obj['values'], obj['weights'], obj['v_min'], obj['v_max'], obj['item_city_assignment'])
 
-def get_problem_instances(json_file_path: str) :
-    with open(json_file_path) as f:
+def get_problem_instances(json_file_path: str) -> list :
+    with open(json_file_path) as f :
         d = json.load(f)
     return [object_decoder(obj) for obj in list(d)]
--- ttp.py
+++ ttp.py
@@ -4,11 +4,7 @@ from math import factorial, pow
 from typing import Callable
 from functools import reduce
 
-def can_item_be_stolen_for_city(city) :
-    pass
-
-def get_weight_for_city(city_index, city_traversal, items_decision_vector) :
-    pass
+import numpy as np
 
 def city_traversal(n: int) -> (Callable[[int], list], Callable[[list], int]) :
     if n < 1 :
@@ -85,45 +81,93 @@ def items_values(n: int) -> (Callable[[int], list], Callable[[list], int]) :
         return k
 
     return integer_to_value_vector, value_vector_to_integer
-            
-        
 
-class TTP :
-    def __init__(self, instance: ProblemInstance) :
-        self.instance: ProblemInstance = instance
-
-    def encode_traversal_into_genotype(permutation: list) :
-        pass
-
-        
+def get_objective_functions(instance: ProblemInstance) :
 
 
-    def decode_genotypy_into_traversal() :
-        pass
+    def get_distances(instance: ProblemInstance) -> list :
+        n = instance.no_cities
+        k = 0
+        distances = [[0 for _ in range(n)] for _ in range(n)]
+        for i in range(0, n) :
+            for j in range(i + 1, n) :
+                distances[i][j] = instance.distances[k]
+                distances[j][i] = instance.distances[k]
+                k += 1
+        return np.array(distances)
 
-    def encode() : 
-        pass
+    distances = get_distances(instance)
+    assignments = np.array(instance.item_city_assignment)
 
 
-    def get_decision_vector() :
-        pass
+    def knapsack_objective(value_vector: list) -> int :
+        if len(instance.values) != len(value_vector) :
+            raise Exception('Incompatible values with value vector length')
 
+        return reduce(lambda cumulated, current : cumulated + (current[1] if value_vector[current[0]] else 0), enumerate(instance.values), 0)
+
+    def traveling_salesman_objective(permutation: list, value_vector: list) -> float :
+        n = instance.no_cities
+
+        def velocity(quantity: int) -> float :
+            return instance.v_max - quantity / instance.capacity * (instance.v_max - instance.v_min)
+
+        def get_weights_until_current_city(partial_permutation: list) -> int :
+            i = len(partial_permutation)
+            # print (partial_permutation)
+            total_weight = 0
+            for k in range(i) :
+                for j in range(n) :
+                    total_weight += value_vector[j] * instance.weights[j] * assignments.item((j, partial_permutation[k]))
+
+            return total_weight
+
+        distance_cost = 0
+        for i in range(n - 1) :
+            distance_cost += distances[permutation[i], permutation[i + 1]] / velocity(get_weights_until_current_city(permutation[:i]))
+
+        distance_cost += distances[permutation[n - 1], permutation[0]] / velocity(get_weights_until_current_city(permutation))
+
+        return distance_cost
+
+    def get_weights(value_vector: list) -> int :
+        if len(instance.weights) != len(value_vector) :
+            raise Exception('Incompatible weights with value vector length')
+
+        return reduce(lambda cumulated, current : cumulated + (current[1] if value_vector[current[0]] else 0), enumerate(instance.weights), 0)
+
+    return knapsack_objective, traveling_salesman_objective, get_weights, distances, assignments
+
+
+
+def multi_objective_genetic_algorithm(instance: ProblemInstance) :
+    pass
 
 if __name__ == '__main__':
     print ('Deserialization started')
-    instaces = get_problem_instances('input_example.json')
-    print (instaces)
+    instances = get_problem_instances('input_example.json')
 
-    encoder, decoder = city_traversal(3)
+    for instance in instances :
 
-    for i in range(factorial(3)) :
-        perm = encoder(i)
-        print (perm)
-        print (decoder(perm))
+        encoder, decoder = city_traversal(instance.no_cities - 1)
+        encoder2, decoder2 = items_values(instance.no_items)
 
-    encoder2, decoder2 = items_values(3)
-    for i in range(2 ** 3) :
-        val_vec = encoder2(i)
-        print (val_vec)
-        print (decoder2(val_vec))
+        ko, tso, gw, dist, ica = get_objective_functions(instance)
+
+        for i in range(factorial(instance.no_cities - 1)) :
+            for j in range(2 ** instance.no_items) :
+                perm = encoder(i)
+                val_vec = encoder2(j)
+                
+                print (perm)
+                print (val_vec)
+                print (tso(perm, val_vec))
+
+        # print (dist)
+        # print (ica)
+
+        # for i in range(2 ** instance.no_items) :
+        #     print (val_vec)
+        #     print (ko(val_vec))
+
 
