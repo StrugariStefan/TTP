8322812b631c54813855f88fd969508fc17d7247
Strugari, Stefan <Stefan.Strugari@centric.eu>
2020-03-21 02:18:17 +0200

    Add operators

--- ttp.py
+++ ttp.py
@@ -1,14 +1,38 @@
 from reader import get_problem_instances, ProblemInstance
 from itertools import permutations
-from math import factorial, pow
+from math import factorial, pow, ceil, log2
 from typing import Callable
 from functools import reduce
 
 import numpy as np
 
-def city_traversal(n: int) -> (Callable[[int], list], Callable[[list], int]) :
+def city_traversal(n: int) -> (Callable[[int], list], Callable[[list], int], Callable[[int], list], Callable[[list], int]) :
     if n < 1 :
         raise Exception('Invalid Permutation Length')
+
+    no_of_bits = ceil(log2(factorial(n)))
+
+    def integer_to_boolean_vector(k: int) -> list :
+        if k >= factorial(n) :
+            raise Exception('This does not reprezint a valid permutation')
+
+        boolean_vector = [False for _ in range(no_of_bits)]
+        for i in reversed(range(no_of_bits)) :
+            boolean_vector[i] = k % 2 == 1
+            k = k // 2
+
+        return boolean_vector
+
+    def boolean_vector_to_integer(boolean_vector: list) -> int :
+        if len(boolean_vector) != no_of_bits :
+            raise Exception('Invalid boolean vector') 
+        
+        k = 0
+        for i in range(no_of_bits) :
+            if boolean_vector[n - i - 1] :
+                k += 2 ** i
+
+        return k
 
     def integer_to_permutation(k: int) -> list:
         if k >= factorial(n) :
@@ -52,7 +76,7 @@ def city_traversal(n: int) -> (Callable[[int], list], Callable[[list], int]) :
 
         return k
 
-    return integer_to_permutation, permutation_to_integer
+    return integer_to_permutation, permutation_to_integer, integer_to_boolean_vector, boolean_vector_to_integer
 
 def items_values(n: int) -> (Callable[[int], list], Callable[[list], int]) :
     if n < 1 :
@@ -83,7 +107,6 @@ def items_values(n: int) -> (Callable[[int], list], Callable[[list], int]) :
     return integer_to_value_vector, value_vector_to_integer
 
 def get_objective_functions(instance: ProblemInstance) :
-
 
     def get_distances(instance: ProblemInstance) -> list :
         n = instance.no_cities
@@ -150,9 +173,98 @@ def get_objective_functions(instance: ProblemInstance) :
 
     return knapsack_objective, traveling_salesman_objective, get_weights, distances, assignments
 
-def NSGA_II(instance: ProblemInstance) :
-    ko, tso, w, d, a = get_objective_functions(instance)
+def get_random_population(size: int, n: int, m: int) -> list :
+    population = []
+    for i in range(size) :
+        permutation_repr = np.random.randint(low = 0, high = factorial(n - 1), size = None)
+        value_vector_repr = np.random.randint(low = 0, high = 2 ** m, size = None)
+        population.append((permutation_repr, value_vector_repr))
+
+    return population
+
+def crossover(individuals: tuple) -> tuple:
+    o1 = individuals[0][:]
+    o2 = individuals[1][:]
+
+    if (len(o1) != len(o2)) :
+        raise Exception("Parents do not have same reprezentation")
+
+    n = len(o1)
+    slices = np.random.choice(range(n), size = 2, replace = False)
+    slices = np.sort(slices)
+
+    aux = o1[slices[0] : slices[1]]
+    o1[slices[0] : slices[1]] = o2[slices[0] : slices[1]]
+    o2[slices[0] : slices[1]] = aux
+
+    return o1, o2
+
+def mutation(individual: list, mutation_rate: float) -> list :
+    mutated_individual = [False for _ in range(len(individual))]
+    for index, gene in enumerate(individual) :
+        if np.random.uniform(low = 0.0, high = 1.0, size = None) <= mutation_rate :
+            mutated_individual[index] = not gene
+        else :
+            mutated_individual[index] = gene
     
+    return mutated_individual
+
+def apply_operators(population: list, crossover: Callable[[tuple], tuple], mutation: Callable[[list], list]) -> list :
+    n = len(population)
+    offspring = []
+    
+    np.random.shuffle(population)
+
+    for index in range(0, n, 2) :
+        o1, o2 = crossover((population[index], population[index + 1]))
+        offspring.append(o1)
+        offspring.append(o2)
+
+    return offspring
+
+
+def NSGA_II(instance: ProblemInstance) :
+    n = instance.no_cities
+    m = instance.no_items
+
+    ko, tso, w, d, a = get_objective_functions(instance)
+    dist_encoder, dist_decoder, dist_into_boolean_vector, boolean_vector_to_dist = city_traversal(n - 1)
+    item_encoder, item_decoder = items_values(m)
+
+    size = int(5e+1)
+
+    p = get_random_population(size, n, m)
+
+    encoded_population = list(map(lambda element: dist_into_boolean_vector(element[0]) + item_encoder(element[1]), p))
+    encoded_offspring = apply_operators(encoded_population, crossover, mutation)
+
+    print (encoded_offspring[0])
+    t = 0
+    while t < 10:
+        t += 1
+    # for e in p:
+    #     break
+
+    # integer = e[1]
+    # print (integer)
+    # value_vector = item_encoder(integer)
+    # print (value_vector)
+    # new_integer = item_decoder(value_vector)
+    # print (new_integer)
+
+    # o1, o2 = crossover((value_vector, item_encoder(21)))
+    # print (o1)
+    # print (o2)
+
+    # mi = mutation(value_vector, 0.5)
+    # print (mi)
+
+    # integer = e[0]
+    # print (integer)
+    # boolean_vector = dist_into_boolean_vector(integer)
+    # print (boolean_vector)
+    # decoded_integer = boolean_vector_to_dist(boolean_vector)
+    # print (decoded_integer)
 
 
 
@@ -165,18 +277,19 @@ if __name__ == '__main__':
 
     for instance in instances :
 
-        encoder, decoder = city_traversal(instance.no_cities - 1)
-        encoder2, decoder2 = items_values(instance.no_items)
+        NSGA_II(instance)
+        # encoder, decoder = city_traversal(instance.no_cities - 1)
+        # encoder2, decoder2 = items_values(instance.no_items)
 
-        ko, tso, gw, dist, ica = get_objective_functions(instance)
+        # ko, tso, gw, dist, ica = get_objective_functions(instance)
 
-        for i in range(factorial(instance.no_cities - 1)) :
-            for j in range(2 ** instance.no_items) :
-                perm = encoder(i)
-                val_vec = encoder2(j)
+        # for i in range(factorial(instance.no_cities - 1)) :
+        #     for j in range(2 ** instance.no_items) :
+        #         perm = encoder(i)
+        #         val_vec = encoder2(j)
                 
-                tso(perm, val_vec)
-                ko(val_vec)
+        #         tso(perm, val_vec)
+        #         ko(val_vec)
                 # print ('--------------')
                 # print (perm)
                 # print (val_vec)
